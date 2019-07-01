c536844774c806a1ffc604af118826f7f136e66a
Mauricio Piñon Islas <mauricio_2612@live.com.mx>
2019-07-01 04:49:26 -0500

    More Styles was added

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,16 +10,22 @@ class Card extends React.Component {
                     <img id="imgChar"alt={this.props.name} src= {this.props.image}></img>
                 </div>
                 <div id="dataCard">
-                    <h3>{this.props.name}</h3>
-                    <p><span>Character ID:</span>{this.props.IDC}</p>
+                    <h2>{this.props.name}</h2>
+                    <p><span>Character ID:</span>   {this.props.IDC}</p>
                     <hr></hr>
-                    <p><span>Status:</span>{this.props.status}</p>
-                    <p><span>Specie:</span>{this.props.specie}</p>
-                    <p><span>Gender:</span>{this.props.gender}</p>
-                    <p><span>Origin:</span>{this.props.origin}</p>
-                    <p><span>Location:</span>{this.props.location}</p>
-                    <p><span>Type:</span>{this.props.type}</p>
-                    <p><span>Created at:</span>{moment(this.props.created).format('MMM Do YYYY')}</p>
+                    <p><span>Status:</span>     {this.props.status}</p>
+                    <hr></hr>
+                    <p><span>Specie:</span>     {this.props.specie}</p>
+                    <hr></hr>
+                    <p><span>Gender:</span>     {this.props.gender}</p>
+                    <hr></hr>
+                    <p><span>Origin:</span>     {this.props.origin}</p>
+                    <hr></hr>
+                    <p><span>Location:</span>   {this.props.location}</p>
+                    <hr></hr>
+                    <p><span>Type:</span>       {this.props.type}</p>
+                    <hr></hr>
+                    <p><span>Created at:</span> {moment(this.props.created).format('MMM Do YYYY')}</p>
                 </div>
 
             </div>
